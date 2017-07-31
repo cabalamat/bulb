480a5a3ba168138c6d38049c5deb67ca3edca832
Phil Hunt <cabalamat@gmail.com>
2017-08-01 00:30:18 +0100

    envir

--- envir.py
+++ envir.py
@@ -1,7 +1,24 @@
 # envir.py = environment for FP
 
+from typing import *
+
 from fp import FunPred
 
+""" A LogItem contains (x,y,p) where:
+- x: input to BB Function
+- y; output from BB function (or None if not available)
+- p: prediction by FP of y (or None if not available)
+"""
+LogItem = Tuple[str, Optional[str], Optional[str]]
+
+""" A Log is a list of Logitems.
+These are in chronological order. The last one, which will be
+the FP's task, is therefore given by logItem[-1].
+"""
+Log = List[LogItem]
+
+""" the name of a BB Function is a string """
+BBFName = str
 
 #---------------------------------------------------------------------
 """
@@ -12,13 +29,16 @@ An environment for a Function predictor
 class Environment:
     
     def __init__(self, fp:FunPred):
-        self.fp = fp
+        self.fp = fp    
         
-    def getTask(self):
+    def getLog(self)->Log:
         """ The FP asks the environment what its current task is.
-        The reply includes information of previous input/output of BB 
-        functions.
+        The reply includes information of previous input/output of the
+        current BB function.
         """
+        
+    def getHistory(self)->    
+        
         
     def makePrediction(self, bbfn:str, x:str, p:str)->str:
         """ The FP informs the environment of its prediction of
